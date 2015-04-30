79b4863539bb14e0f31f4fda797b94dad8521862
Corey Leigh Latislaw <colabug@gmail.com>
2015-04-30 07:19:13 -0400

    Added card tests

--- app/src/main/java/com/greenlifesoftware/simple1/CardFragment.java
+++ app/src/main/java/com/greenlifesoftware/simple1/CardFragment.java
@@ -6,6 +6,7 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 public class CardFragment extends Fragment
 {
@@ -16,6 +17,17 @@ public class CardFragment extends Fragment
                               Bundle savedInstanceState )
     {
         View layout = inflater.inflate( R.layout.fragment_card, container );
+        layout.findViewById( R.id.toast_button ).setOnClickListener( new View.OnClickListener()
+        {
+            @Override
+            public void onClick( View v )
+            {
+                Toast.makeText( getActivity(),
+                                getString( R.string.CARD_TOAST ),
+                                Toast.LENGTH_SHORT )
+                     .show();
+            }
+        } );
 
         return layout;
     }
--- robolectric_tests/src/test/java/com/greenlifesoftware/simple1/CardFragmentTest.java
+++ robolectric_tests/src/test/java/com/greenlifesoftware/simple1/CardFragmentTest.java
@@ -1,19 +1,68 @@
 package com.greenlifesoftware.simple1;
 
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import com.greenlifesoftware.support.ResourceLocator;
 import com.greenlifesoftware.support.RobolectricGradleTestRunner;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static com.greenlifesoftware.support.ResourceLocator.getString;
+import static org.hamcrest.CoreMatchers.equalTo;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertThat;
+import static org.robolectric.shadows.ShadowToast.getTextOfLatestToast;
+import static org.robolectric.util.FragmentTestUtil.startFragment;
 
 @RunWith(RobolectricGradleTestRunner.class)
 public class CardFragmentTest
 {
+    private CardFragment fragment;
+    private Button toastButton;
+
+    @Before
+    public void setUp() throws Exception
+    {
+        fragment = CardFragment.newInstance();
+        startFragment( fragment );
+        toastButton = (Button) getViewById( R.id.toast_button );
+    }
+
     @Test
     public void shouldNotBeNull() throws Exception
     {
-        CardFragment fragment = CardFragment.newInstance();
         assertNotNull( fragment );
+    }
+
+    @Test
+    public void shouldHaveCardImage() throws Exception
+    {
+        ImageView card = (ImageView) getViewById( R.id.card_image );
+        assertNotNull( card );
+        assertThat( card.getDrawable(),
+                    equalTo( ResourceLocator.getDrawable( R.drawable.ace ) ) );
+    }
+
+    @Test
+    public void shouldHaveToastButton() throws Exception
+    {
+        assertNotNull( toastButton );
+    }
+
+    @Test
+    public void toastButtonShouldToast() throws Exception
+    {
+        toastButton.performClick();
+        assertThat( getTextOfLatestToast(),
+                    equalTo( getString( R.string.CARD_TOAST ) ) );
+    }
+
+    private View getViewById( int id )
+    {
+        return fragment.getView().findViewById( id );
     }
 }

3b9d3cedc0d5e2fe11ad7f0bccc966a4154b246b
Corey Leigh Latislaw <colabug@gmail.com>
2015-04-30 06:57:09 -0400

    Finished tests for SumFragment

--- app/src/main/java/com/greenlifesoftware/simple1/SumFragment.java
+++ app/src/main/java/com/greenlifesoftware/simple1/SumFragment.java
@@ -6,16 +6,28 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 public class SumFragment extends Fragment
 {
-    @Nullable
-    @Override
+    @Nullable @Override
     public View onCreateView( LayoutInflater inflater,
                               ViewGroup container,
                               Bundle savedInstanceState )
     {
         View layout = inflater.inflate( R.layout.fragment_sum, container );
+        final TextView display = (TextView) layout.findViewById( R.id.sum_display );
+
+        layout.findViewById( R.id.add_button ).setOnClickListener( new View.OnClickListener()
+        {
+            @Override
+            public void onClick( View v )
+            {
+                int sum = 2 + 3;
+                display.setText("" + sum);
+            }
+        } );
+
         return layout;
     }
 
--- robolectric_tests/src/test/java/com/greenlifesoftware/simple1/SumFragmentTest.java
+++ robolectric_tests/src/test/java/com/greenlifesoftware/simple1/SumFragmentTest.java
@@ -1,20 +1,70 @@
 package com.greenlifesoftware.simple1;
 
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
 import com.greenlifesoftware.support.RobolectricGradleTestRunner;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static com.greenlifesoftware.support.ResourceLocator.*;
+import static org.hamcrest.CoreMatchers.equalTo;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertThat;
+import static org.robolectric.util.FragmentTestUtil.startFragment;
 
 @RunWith(RobolectricGradleTestRunner.class)
 
 public class SumFragmentTest
 {
+    public static final String SUM_TOTAL = "5";
+    private SumFragment fragment;
+    private Button addButton;
+    private TextView display;
+
+    @Before
+    public void setUp() throws Exception
+    {
+        fragment = SumFragment.newInstance();
+        startFragment( fragment );
+        addButton = (Button) getViewById( R.id.add_button );
+        display = (TextView) getViewById( R.id.sum_display );
+    }
+
     @Test
     public void shouldNotBeNull() throws Exception
     {
-        SumFragment fragment = SumFragment.newInstance();
         assertNotNull( fragment );
+    }
+
+    @Test
+    public void shouldHaveAddButton() throws Exception
+    {
+        assertNotNull( addButton );
+        assertThat( addButton.getText().toString(),
+                    equalTo( getString( R.string.ADD_BUTTON_TEXT ) ) );
+    }
+
+    @Test
+    public void shouldHaveSumDisplay() throws Exception
+    {
+        assertNotNull( display );
+        assertThat( display.getText().toString(),
+                    equalTo( getString( R.string.DEFAULT_SUM_DISPLAY ) ) );
+    }
+
+    @Test
+    public void shouldAddOnClick() throws Exception
+    {
+        addButton.performClick();
+        assertThat( display.getText().toString(), equalTo( SUM_TOTAL ) );
+    }
+
+    private View getViewById( int id )
+    {
+        return fragment.getView().findViewById( id );
     }
 }

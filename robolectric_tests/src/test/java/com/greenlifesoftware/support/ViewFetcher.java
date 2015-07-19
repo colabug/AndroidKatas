6a3714768fac74d71f8ed1eca77f5886738e3ad2
Corey Leigh Latislaw <colabug@gmail.com>
2015-07-19 08:30:10 -0400

    Added view fetching

---
+++ robolectric_tests/src/test/java/com/greenlifesoftware/support/ViewFetcher.java
@@ -0,0 +1,24 @@
+package com.greenlifesoftware.support;
+
+import android.app.Fragment;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class ViewFetcher
+{
+    public static View getView( Fragment fragment, int viewId )
+    {
+        return fragment.getView().findViewById( viewId );
+    }
+
+    public static TextView getTextView( Fragment fragment, int viewId )
+    {
+        return (TextView) getView( fragment, viewId );
+    }
+
+    public static Button getButton( Fragment fragment, int viewId )
+    {
+        return (Button) getView( fragment, viewId );
+    }
+}

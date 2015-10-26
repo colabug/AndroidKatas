49fb870869c0554755411ca8ddd0b0ef8b8e3605
Erik Hellman <erik.hellman@bontouch.com>
2015-10-26 10:13:37 +0100

    Updated to Robolectric 3.0

--- app/src/main/java/com/greenlifesoftware/calculator/CalculatorActivity.java
+++ app/src/main/java/com/greenlifesoftware/calculator/CalculatorActivity.java
@@ -1,14 +1,12 @@
 package com.greenlifesoftware.calculator;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 
-public class CalculatorActivity extends ActionBarActivity
-{
+public class CalculatorActivity extends ActionBarActivity {
     @Override
-    protected void onCreate( Bundle savedInstanceState )
-    {
-        super.onCreate( savedInstanceState );
-        setContentView( R.layout.activity_calculator );
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calculator);
     }
 }
